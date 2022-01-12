ebf56637aa51e27afade2c0831cbf43c35100cc6
Alex <alexnb.bg@gmail.com>
2022-01-12 11:37:55 +0200

    test3

--- _main.py
+++ _main.py
@@ -40,6 +40,20 @@ def _main_menu():
 
 
 
+'''
+def pr():
+    print('asd')
+
+
+a = {
+    1: pr,
+    2: 'asd'
+
+}
+
+user_input = int(input('Enter option number: '))
+eval(a[user_input]())
+'''
 
 
 
